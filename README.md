a762ec1a52eb1caae8dfd7ed950a676d4530284a
lucas-leoni <lucas.leoni09@gmail.com>
2022-06-21 14:23:53 -0300

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# gift
+# desapegaqui
 
 ## Project setup
 ```
--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -5,8 +5,8 @@
         <div class="col-5 d-flex flex-column justify-content-center">
           <h1>Plataforma Digital de Doações</h1>
           <p class="fs-3 pt-5">
-            GIFT é uma plataforma que realiza a intermediação da comunicação entre pessoas que
-            possuem itens para doar com pessoas ou entidades que buscam contribuições.
+            Desapegaqui é uma plataforma que realiza a intermediação da comunicação entre pessoas
+            que possuem itens para doar com pessoas ou entidades que buscam contribuições.
           </p>
         </div>
         <div class="col-5">
@@ -20,7 +20,7 @@
           <img src="../../assets/image-2.svg" alt="image-2" width="100%" height="100%" />
         </div>
         <div class="col-5 d-flex flex-column justify-content-center">
-          <h1>Vitrine Virtual de Doações e Necessidades</h1>
+          <h1>Vitrine Virtual de Doações</h1>
           <p class="fs-3 pt-5">
             Doadores encontram pessoas ou entidades para realizar suas doações de forma simples e
             rápida.
