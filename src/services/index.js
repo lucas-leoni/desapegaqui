05f9a2ed76a4c6bdf5c2f4dd1652716b43ecfe09
lucas-leoni <lucas.leoni09@gmail.com>
2022-06-30 19:34:48 -0300

    update

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
+/* import { createClient } from '@supabase/supabase-js';
 
 const URL = '';
 const KEY = '';
 const SUPABASE_AUTH = createClient(URL, KEY);
 
-export default SUPABASE_AUTH;
+export default SUPABASE_AUTH; */
