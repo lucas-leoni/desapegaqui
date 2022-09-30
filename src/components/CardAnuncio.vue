e32c125d75adc485005fa292a8550231c66025bd
lucas-leoni <lucas.leoni09@gmail.com>
2022-09-30 17:26:53 -0300

    update

--- src/components/CardAnuncio.vue
+++ src/components/CardAnuncio.vue
@@ -1,16 +1,14 @@
 <template>
-  <div class="col-3 d-flex justify-content-center mb-4">
-    <div class="card" style="width: 18rem">
-      <img src="../assets/logo-nova.png" class="card-img-top" alt="img-anuncio" />
-      <div class="card-body">
-        <h5 class="card-title">Título</h5>
-        <p class="card-text">Descrição</p>
-        <div class="col d-flex justify-content-between">
-          <router-link to="" class="btn btn-primary"> Mais detalhes </router-link>
-          <router-link to="" class="btn favorito">
-            <i class="bi bi-heart text-white"></i>
-          </router-link>
-        </div>
+  <div class="card" style="width: 18rem">
+    <img src="../assets/logo-nova.png" class="card-img-top" alt="img-anuncio" />
+    <div class="card-body">
+      <h5 class="card-title">{{ announcement.title }}</h5>
+      <p class="card-text">{{ announcement.description }}</p>
+      <div class="col d-flex justify-content-between">
+        <router-link to="" class="btn btn-primary"> Mais detalhes </router-link>
+        <router-link to="" class="btn favorito">
+          <i class="bi bi-heart text-white"></i>
+        </router-link>
       </div>
     </div>
   </div>
@@ -19,6 +17,12 @@
 <script>
 export default {
   name: 'CardAnuncio',
+  props: {
+    announcement: {
+      type: Object,
+      required: true,
+    },
+  },
 };
 </script>
 
