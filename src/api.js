1e617bded80f29362206e77e36f037e72137ddfd
lucas-leoni <lucas.leoni09@gmail.com>
2022-12-07 20:28:02 -0300

    update

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  /* baseURL: 'http://localhost:5000/api', */
+  baseURL: 'http://192.168.30.19:5000/api',
 });
 
 export default api;
