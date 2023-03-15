12c88e7718aa484f5a0eb224337e9495944d1e40
lucas-leoni <lucas.leoni09@gmail.com>
2023-03-15 18:53:45 -0300

    feat: logrocket added

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
 import Vuelidate from 'vuelidate';
+import LogRocket from 'logrocket';
 import App from './App.vue';
 import router from './router';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+LogRocket.init('v2ujcx/desapegaqui');
 
 Vue.use(Vuelidate);
 
